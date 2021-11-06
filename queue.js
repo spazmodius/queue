2c45bc3f667659c533fd7434210c5a51467c9385
spazmodius <rajiv@delwadia.com>
2021-11-05 20:04:56 -0400

    simpler a little faster

--- queue.js
+++ queue.js
@@ -1,82 +1,53 @@
 'use strict'
 
-function Queue() {
-	let length = 0
-	let head = Array(), headIndex = 0
-	let tail = head, tailIndex = 0
-
-	head.next = Array()
-	head.next.next = head
-
-	//  same block, headIndex === 0
-	const ABC = {
-		enqueue(value) {
-			tail[ tailIndex++ ] = value
-			return ++length
-		},
-		dequeue() {
-			if (length === 0)
-				return undefined
-			const value = head[ headIndex ]
-			head[ headIndex++ ] = undefined
-			if (--length === 0)
-				headIndex = tailIndex = 0
-			else if (tailIndex < head.length)
-				state = E
-			else {
-				tail = head.next
-				tailIndex = 0
-				state = KMO
-			}
-			return value
-		}
-	}
-
-	// same block, headIndex > 0
-	const E = {
-		enqueue(value) {
-			tail[ tailIndex++ ] = value
-			if (tailIndex === tail.length) {
-				tail = tail.next
-				tailIndex = 0
-				state = KMO
-			}
-			return ++length
-		},
-		dequeue() {
-			const value = head[ headIndex ]
-			head[ headIndex++ ] = undefined
-			if (--length === 0) {
-				headIndex = tailIndex = 0
-				state = ABC
-			}
-			return value
-		}
-	}
-
-	// different blocks
-	const KMO = {
-		enqueue: ABC.enqueue,
-		dequeue() {
-			const value = head[ headIndex ]
-			head[ headIndex++ ] = undefined
-			--length
-			if (headIndex === head.length) {
-				head = head.next
-				headIndex = 0
-				state = ABC
-			}
-			return value
-		}
-	}
-
-	let state = ABC
-
-	return {
-		enqueue(value) { return state.enqueue(value) },
-		dequeue() { return state.dequeue() },
-		get length() { return length },
-	}
+function Block() {
+	const block = Array(32)
+	block.next = null
+	return block
 }
 
-module.exports = Queue
+class _Queue {
+	constructor() {
+		this._length = 0
+		this._head = Block()
+		this._tail = this._head
+		this._hx = 0
+		this._tx = 0
+	}
+
+	enqueue(value) {
+		if (this._tx === this._tail.length) {
+			this._tail = this._tail.next ?? (this._tail.next = Block())
+			this._tx = 0
+		}
+		this._tail[ this._tx++ ] = value
+		return ++this._length
+	}
+
+	dequeue() {
+		if (this._length === 0)
+			return undefined
+
+		const value = this._head[ this._hx ]
+		this._head[ this._hx ] = 0
+
+		if (--this._length === 0) {
+			this._hx = this._tx = 0
+		}
+		else if (++this._hx === this._head.length) {
+			const block = this._head
+			this._head = block.next
+			this._hx = 0
+			block.next = null
+			this._tail.next = block
+		}
+
+		return value
+	}
+
+	get length() { return this._length }
+}
+
+module.exports = function Queue() {
+	return new _Queue()
+}
